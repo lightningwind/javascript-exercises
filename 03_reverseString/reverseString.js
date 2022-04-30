c057c8581f5544c219822566274a1f10b61242c6
David Islam <david.islam24@gmail.com>
2022-04-29 20:21:46 -0700

    Complete Exercise 03 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,33 @@
-const reverseString = function() {
-
+/* Returns a string that is equal to the 
+input string, reversed. */
+const reverseString = function(str) {
+    /*
+    Approach #1:
+        - intuition: Since strings in JS
+        are immutable, we first convert 
+        input string into its array equivalent.
+        Then, using two pointers, we reverse
+        the array. Finally, we convert the array
+        back into a string and return the answer.
+    */
+   let arr = str.split('');
+   reverseArray(arr);
+   return arr.join('');
 };
+
+/* Reverses array <array>. */
+function reverseArray(array) { // O(n)
+    let i = 0;
+    let j = array.length - 1;
+    while (i < j) {
+        // Swap values
+        const tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+        i++;
+        j--;
+    }
+}
 
 // Do not edit below this line
 module.exports = reverseString;
