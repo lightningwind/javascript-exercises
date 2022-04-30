c86e02c3a05c6da57b0984d2da4ec617eda5d6ec
David Islam <david.islam24@gmail.com>
2022-04-30 16:07:20 -0700

    Complete Exercise 6 - leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+/* Returns true iff the year <year>
+is a valid leap year. */
+const leapYears = function(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || 
+        (year % 100 === 0 && year % 400 === 0)) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
