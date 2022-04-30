2494222d69c4e9c905554b7f2629146ab038206d
David Islam <david.islam24@gmail.com>
2022-04-30 15:07:02 -0700

    Improve solution to removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+/* Returns a new array containing those elements
+in array <array> not present in rest param <args>. */
+const removeFromArray = function(array, ...args) {
+    return array.filter(ele => !args.includes(ele));
 };
 
 // Do not edit below this line
