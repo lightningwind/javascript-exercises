3655fffc19b4cb8a4ebf59f2892f9b67b9e328c9
David Islam <david.islam24@gmail.com>
2022-04-30 16:33:06 -0700

    Complete Exercise 7 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const converted = (temp - 32) * (5 / 9);
+  return Math.round(converted * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  const converted = temp * (9 / 5) + 32;
+  return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
