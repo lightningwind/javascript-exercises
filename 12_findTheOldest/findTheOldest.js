12f7ff6b5d18d0153275266ac4c73cc894895f31
David Islam <david.islam24@gmail.com>
2022-09-12 09:46:36 -0700

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((prevPerson, curPerson) => {
+        const currentYear = (new Date()).getFullYear();
+        const prevPersonAge = prevPerson.yearOfDeath ? 
+                              prevPerson.yearOfDeath - prevPerson.yearOfBirth : 
+                              currentYear - prevPerson.yearOfBirth;
+        const curPersonAge = curPerson.yearOfDeath ? 
+                             curPerson.yearOfDeath - curPerson.yearOfBirth :
+                             currentYear - curPerson.yearOfBirth;
+        return prevPersonAge < curPersonAge ? curPerson : prevPerson;
+    })
 };
+
+// Useful array methods: sort(), reduce(), 
 
 // Do not edit below this line
 module.exports = findTheOldest;
