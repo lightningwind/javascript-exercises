befac1cc8588efcc78934da6e6f28bf7be42ffda
David Islam <david.islam24@gmail.com>
2022-09-12 08:31:19 -0700

    Solve Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n <= 0) {
+        return "OOPS";
+    }
+    let a = 1;
+    let b = 1;
+    for (let i = 1; i < n; i++) {
+        const c = a + b;
+        a = b;
+        b = c; 
+    }
 
+    return a;
 };
 
 // Do not edit below this line
